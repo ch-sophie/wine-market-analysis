e61fe91f23b6196561c1604c956fe85d11705575
ch-sophie <chengsophie25@gmail.com>
2026-03-31 14:27:46 +0200

    commit top 10

--- scripts/highlight_10.sql
+++ scripts/highlight_10.sql
@@ -14,3 +14,25 @@ JOIN vintages v ON w.id = v.wine_id
 WHERE w.ratings_count > 1000  -- Ensuring social proof
 ORDER BY w.ratings_count DESC
 LIMIT 10;
+
+-------------------------------------------
+-- Get top 10 wines and group by so it's different wines - not the same wine 2015, 2016, 2017
+SELECT
+    w.name,
+    c.name AS country,
+    w.ratings_average,
+    w.ratings_count,
+    AVG(v.price_euros) AS avg_price,
+    MIN(v.price_euros) AS entry_price,
+    MAX(v.year) AS most_recent_vintage,
+    COUNT(v.id) AS vintage_count,
+    (w.ratings_average * LOG(w.ratings_count)) AS score
+FROM wines w
+JOIN regions r ON w.region_id = r.id
+JOIN countries c ON r.country_code = c.code
+LEFT JOIN vintages v ON v.wine_id = w.id
+AND v.price_euros IS NOT NULL
+WHERE w.ratings_count > 1000
+GROUP BY w.id, w.name, w.ratings_average, w.ratings_count, c.name
+ORDER BY score DESC
+LIMIT 10;
